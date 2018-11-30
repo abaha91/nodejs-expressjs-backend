b29d6291df54f388ae3d231b5ac079380d5e20f6
Gennadiy <g.abashkin@petrovich.ru>
2018-11-30 18:59:31 +0300

    Installed lowdb added static files source folder to express in app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 app.set('views', './source/views/pages');
 app.set('view engine', 'pug');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./source/routes/index'));
 
 const server = app.listen(process.env.PORT || 3000, () => {
